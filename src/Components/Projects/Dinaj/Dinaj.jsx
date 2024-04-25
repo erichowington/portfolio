8e67318ef77239ec55c688c0cd47618821dd1f7e
erichowington <ericehowington@gmail.com>
2024-04-25 13:55:14 -0400

    dinaj ui images and css

--- src/Components/Projects/Dinaj/Dinaj.jsx
+++ src/Components/Projects/Dinaj/Dinaj.jsx
@@ -1,67 +1,56 @@
-// import React from "react";
-// import { useState, useEffect } from "react";
-// import "./Dinaj.css";
-
-// function Dinaj() {
-//   const [dinajModal, setDinajModal] = useState(false);
-
-//   const toggleDinajModal = (e) => {
-//     e.stopPropagation(); // Prevent event propagation
-//     setDinajModal(!dinajModal);
-//   };
-
-//   useEffect(() => {
-//     if (dinajModal) {
-//       document.body.classList.add("active-modal");
-//     } else {
-//       document.body.classList.remove("active-modal");
-//     }
-//   }, [dinajModal]);
-
-//   return (
-//     <div className="dinaj-wrapper">
-//         <div className="dinaj-logo-container">
-//             <img
-//                 className="open-dinaj"
-//                 onClick={toggleDinajModal}
-//                 src="https://github.com/erichowington/DoINeedAJacket/blob/main/public/images/Header_TRNSPRNT.png?raw=true"
-//             />
-//       </div>
-//       {dinajModal && (
-//         <div className="dinaj-modal">
-//           <div onClick={toggleDinajModal} className="overlay"></div>
-//           <div className="dinaj-modal-content">
-//             <p>dinaj INFO</p>
-//             <p>dinaj SCREEN SHOTS</p>
-//             <p>dinaj GIT HUB LINK</p>
-//             <div className="close-dinaj-modal" onClick={toggleDinajModal}>
-//               CLOSE
-//             </div>
-//           </div>
-//         </div>
-//       )}
-//     </div>
-//   );
-// }
-
-// export default Dinaj;
-
-
 import React from "react";
 import "./Dinaj.css";
 
 function Dinaj() {
-    return (
-        <div className="Dinaj-wrapper">
-            <div className="dinaj-content">
-                <p>Dinaj INFO</p>
-                <p>Dinaj SCREEN SHOTS</p>
-                <p>Dinaj GIT HUB LINK</p>
-            </div>
+  return (
+    <div className="Dinaj-wrapper">
+      <div className="dinaj-modal-content">
+        <div className="dinaj-info-logo-container">
+          <img
+            className="dinaj-info-logo"
+            src="https://github.com/erichowington/DoINeedAJacket/blob/main/public/images/Header_TRNSPRNT.png?raw=true"
+          />
         </div>
-    );
+
+        <div className="dinaj-tech-stack-container">
+          <img
+            className="react-logo"
+            src="https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/react-js-icon.png"
+          />
+          <img
+            className="js-logo"
+            src="https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/javascript-programming-language-icon.png"
+          />
+        </div>
+
+        <div className="dinaj-info-container">
+          <p className="dinaj-info">
+            <p className="dinaj-p-1">
+            <strong>"Do I Need a Jacket?"</strong> serves as my response to the typical weather
+            application. Whenever I check the day's forecast, the ubiquitous
+            question arises: 'Do I need a jacket?' But it's not just about
+            whether a jacket is needed; it's also about what type is suitable.
+            </p>
+            <p className="dinaj-p-2">
+            Through geolocation services, the app pinpoints the user's location.
+            Alternatively, users can manually input their city. Leveraging the
+            OpenWeatherAPI, we swiftly determine whether a jacket is necessary
+            based on the current weather conditions. Next, we cross-reference
+            this data with REI's recommended jacket guide to advise users on the
+            appropriate jacket type. 
+            </p>
+            <p className="dinaj-p-3">
+            Users can simply click on the suggested
+            answer to view a sample of the recommended jacket. Moreover, the app
+            generates a comprehensive weather report and presents jacket
+            suggestions across various types and price ranges, ensuring options
+            for every user."
+            </p>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Dinaj;
-
-
