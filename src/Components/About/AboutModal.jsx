df231af6d775a13ef7c9b79a2635f48ee6f5eacb
erichowington <ericehowington@gmail.com>
2024-04-30 15:02:37 -0400

    about modal complete

--- src/Components/About/AboutModal.jsx
+++ src/Components/About/AboutModal.jsx
@@ -1,21 +1,27 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import dogImage from "../../Luke.jpeg";
 import "./AboutModal.css";
 
 function AboutModal() {
   const [modal, setModal] = useState(false);
+  const [showDogImage, setShowDogImage] = useState(false); // Add state for showing the dog image
 
   const toggleModal = () => {
     setModal(!modal);
   };
 
+  const toggleDogImage = () => {
+    setShowDogImage(!showDogImage);
+  };
+
   useEffect(() => {
-    if (modal) {
+    if (modal || showDogImage) {
       document.body.classList.add("active-modal");
     } else {
       document.body.classList.remove("active-modal");
     }
-  }, [modal]);
+  }, [modal, showDogImage]);
 
   return (
     <div className="about-wrapper">
@@ -41,15 +47,17 @@ function AboutModal() {
             </div>
             <div className="about-body-container">
               <p className="about-statment">
-                I am a creatively inclined software engineer with a knack for
-                interpersonal relationships. Throughout my career I worked my
-                way up from an entry level sales associate, to managing the
-                flagship store. I transitioned into the service industry where
-                my vision was recognized by the Michelin Guide. I want to bring
-                my tenacity for learning and my love of working with people to
-                solve technical problems for clients as a way to leverage my
-                full stack development skills while building and maintaining
-                meaningful relationships.
+                As a creatively inclined software engineer with a passion for
+                building interpersonal connections, I've followed a diverse
+                career path. Starting as an entry-level sales associate, I
+                advanced to manage a flagship store before transitioning to the
+                service industry. Upon entering the service industry my vision
+                was quickly recognized by the Michelin Guide, where my
+                contributions led me into a technical management and creative
+                direction role. Now, I'm eager to combine my diverse
+                experiences, tenacity for learning, and expertise in full-stack
+                development to solve technical challenges for clients while
+                fostering meaningful relationships.
               </p>
               <p className="about-statment-2">
                 In my personal life, I've always endeavored to learn as much as
@@ -96,6 +104,15 @@ function AboutModal() {
                 <p className="skill-name">JavaScript</p>
               </div>
 
+              <div className="skill skill-vscode">
+                <img
+                  className="skill-icon"
+                  src="https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/visual-studio-code-icon.png"
+                  alt="VS Code logo"
+                />
+                <p className="skill-name">Visual Studio Code</p>
+              </div>
+
               <div className="skill skill-python">
                 <img
                   className="skill-icon"
@@ -113,7 +130,16 @@ function AboutModal() {
                 />
                 <p className="skill-name">Django</p>
               </div>
-
+              <div className="skill skill-PostreSQL">
+                <img
+                  className="skill-icon"
+                  src="https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/postgresql-icon.png"
+                  alt="PostgreSQL Logo"
+                />
+                <p className="skill-name">PostgreSQL</p>
+              </div>
+            </div>
+            <div className="skills-container">
               <div className="skill skill-MongoDB">
                 <img
                   className="skill-icon"
@@ -121,6 +147,15 @@ function AboutModal() {
                   alt="MongoDB Logo"
                 />
                 <p className="skill-name">MongoDB</p>
+              </div>
+
+              <div className="skill skill-NodeJS">
+                <img
+                  className="skill-icon"
+                  src="https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/node-js-icon.png"
+                  alt="NodeJS Logo"
+                />
+                <p className="skill-name">NodeJS</p>
               </div>
               <div className="skill skill-CSS">
                 <img
@@ -138,6 +173,73 @@ function AboutModal() {
                 />
                 <p className="skill-name">JIRA</p>
               </div>
+              <div className="skill skill-photoshop">
+                <img
+                  className="skill-icon"
+                  src="https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/adobe-photoshop-icon.png"
+                  alt="PhotoShop Logo"
+                />
+                <p className="skill-name">Adobe Photoshop</p>
+              </div>
+              <div className="skill skill-illustrator">
+                <img
+                  className="skill-icon"
+                  src="https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/adobe-illustrator-icon.png"
+                  alt="VS Code logo"
+                />
+                <p className="skill-name">Adobe Illustrator</p>
+              </div>
+            </div>
+            <p className="soft-skills-header">SOFT SKILLS</p>
+            <div className="soft-skills-container">
+              <li className="soft-skills">
+                Craft the vision and guide the business or product strategy,
+                ensuring solutions are recurring and simple.
+              </li>
+              <li className="soft-skills">
+                Effective and transparent communication.
+              </li>
+              <li className="soft-skills">
+                Lead empathetically to successfully align teams on missions or
+                solutions.
+              </li>
+              <li className="soft-skills">
+                Keep the customer as the foremost priority, prioritizing their
+                needs over personal opinions.
+              </li>
+              <li className="soft-skills">
+                Adapt quickly to change and effortlessly devise solutions as
+                challenges arise.
+              </li>
+            </div>
+            <div className="about-closing-container">
+              {showDogImage && (
+                <img className="Luke" src={dogImage} alt="Dog" />
+              )}
+              <p className="about-closing-statment">
+                When I'm not at the computer-- I'm usually hanging with my{" "}
+                <span className="dog-word" onClick={toggleDogImage}>
+                  dog
+                </span>
+                , building something, curating a{" "}
+                <a
+                  className="spotify-link"
+                  href="https://open.spotify.com/playlist/1zHTZF8zxF4V2Vof9ML5u0?si=30f29d2a726f4835"
+                  target="_blank"
+                >
+                  playlist
+                </a>
+                , excercising, or at the grocery store.
+              </p>
+              <p className="about-closing-statment-2">
+                This website was wireframed in{" "}
+                <span className="build">Whimsical</span> and coded in{" "}
+                <span className="build">Visual Studio Code</span> by yours
+                truly. Built with <span className="build">React</span> and,
+                deployed with
+                <span className="build">Netlify</span>. All text is set in the{" "}
+                <span className="build">IBM Plex Sans typeface</span>.
+              </p>
             </div>
           </div>
         </div>
