1134a6f1f50d0d9f95c5c10863f4c6e0dd16ed69
erichowington <ericehowington@gmail.com>
2024-05-08 13:48:00 -0400

    clean code and add media query

--- src/App.js
+++ src/App.js
@@ -1,44 +1,3 @@
-// import ProjectsModal from './Components/Projects/ProjectsModal.jsx';
-// import ContactModal from './Components/Contact/ContactModal.jsx';
-// import AboutModal from './Components/About/AboutModal.jsx';
-// import {useState, useEffect } from 'react';
-// import './App.css';
-
-// function App() {
-//   const [modal, setModal] = useState(false);
-
-//     const toggleModal = () => {
-//         setModal(!modal);
-//     };
-
-//     useEffect(() => {
-//         if (modal) {
-//             document.body.classList.add('active-modal');
-//         } else {
-//             document.body.classList.remove('active-modal');
-//         }
-//     }, [modal]);
-
- 
-
-//   return (
-//     <div className="app-wrapper">
-//       <div className='header-container'>
-//         <div className='header-name'>Eric Howington</div>
-//         <div className='header-title'>Developer and Designer</div>
-//       </div>
-//       <div className='section-container'>
-//         <ProjectsModal toggleModal={toggleModal}/>
-//         <AboutModal />
-//         <ContactModal/>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
 import React from 'react';
 import ProjectsModal from './Components/Projects/ProjectsModal.jsx';
 import ContactModal from './Components/Contact/ContactModal.jsx';
@@ -50,7 +9,7 @@ function App() {
     <div className="app-wrapper">
       <div className='header-container'>
         <div className='header-name'>Eric Howington</div>
-        <div className='header-title'>Developer and Designer</div>
+        <div className='app-header-title'>Developer and Designer</div>
       </div>
       <div className='section-container'>
         <ProjectsModal />
